7d1787879d6e821f6a3ab7e0b076f07c647e26d8
pengfeis <supengfei007@gmail.com>
2015-10-09 23:17:13 +0800

    Init commit, a hello world sample of spring mvc

---
+++ spring-mvc/src/main/java/pengfeis/mvc/sample/HelloController.java
@@ -0,0 +1,16 @@
+package pengfeis.mvc.sample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/a")
+public class HelloController {
+
+	@RequestMapping("/helloWorld")
+	public String helloWorld(Model model) {
+		model.addAttribute("message", "Hello World!");
+		return "helloWorld";
+	}
+}
